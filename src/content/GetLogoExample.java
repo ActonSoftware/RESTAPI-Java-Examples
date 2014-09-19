a6a322412e09b2da83c006555d16cb11b758302a
Hemant <hemant.gupta@act-on.net>
2014-09-19 17:27:45 +0530

    Fixed GET logo and GET logo by id

--- src/content/GetLogoExample.java
+++ src/content/GetLogoExample.java
@@ -17,7 +17,7 @@ public class GetLogoExample
 	public static void main(String[] args)
 		{
 				getLogo();
-				//getLogoById();
+				getLogoById();
 		}
 	public static void getLogo(){
 	try
@@ -25,12 +25,12 @@ public class GetLogoExample
 		String accessToken  = Utility.getAccessToken();
 		Unirest.setHttpClient(Utility.makeClient());
 		Unirest.setDefaultHeader("Authorization", "Bearer " + accessToken);
-		HttpResponse<JsonNode> jsonResponse = Unirest.get(BASE_URL + "logo")
+		HttpResponse<String> jsonResponse = Unirest.get(BASE_URL + "logo")
 				.header("accept", "application/json")
-				.asJson();
+				.asString();
 
 		if(jsonResponse.getCode()==200){
-			JSONObject jsonResponseBody = jsonResponse.getBody().getObject();
+			String jsonResponseBody = jsonResponse.getBody();
 			System.out.println("The response body is : "+jsonResponseBody );
 		}else {
 			System.out.println("The response is : "+ jsonResponse);
@@ -49,16 +49,15 @@ public class GetLogoExample
 		{
 		String accessToken  = Utility.getAccessToken();
 		Unirest.setHttpClient(Utility.makeClient());
-		// provide logoid
 		String id = "<Provide Logo Id>";
 		Unirest.setHttpClient(Utility.makeClient());
 		Unirest.setDefaultHeader("Authorization", "Bearer " +accessToken );
-		HttpResponse jsonResponse = Unirest.get(BASE_URL + "api/1/logo/"+id)
+		HttpResponse<String> jsonResponse = Unirest.get(BASE_URL + "logo/"+id)
 				.header("accept", "application/json")
-				.asJson();
+				.asString();
 
 		if(jsonResponse.getCode()==200){
-			JSONObject jsonResponseBody = ((JsonNode)jsonResponse.getBody()).getObject();
+			String jsonResponseBody = jsonResponse.getBody();
 			System.out.println("The response body is : "+jsonResponseBody );
 		}else {
 			System.out.println("The response is : "+ jsonResponse);
