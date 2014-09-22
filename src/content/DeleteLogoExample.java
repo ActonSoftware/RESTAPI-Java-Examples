3ef4d75e86090638efc280684715bc996d09611a
Hemant <hemant.gupta@act-on.net>
2014-09-22 14:14:58 +0530

    Fixed DELETE Logo by id

--- src/content/DeleteLogoExample.java
+++ src/content/DeleteLogoExample.java
@@ -12,7 +12,7 @@ import util.Utility;
  */
 public class DeleteLogoExample
 {
-	static String BASE_URL         = "https://restapi.actonsoftware.com/";
+	static String BASE_URL         = "https://restapi.actonsoftware.com/api/1/";
 	public static void main(String[] args)
 		{
 			deleteLogo();
@@ -25,13 +25,13 @@ public class DeleteLogoExample
 			String accessToken  = Utility.getAccessToken();
 			String id = "<Provide logo id>";
 			Unirest.setHttpClient(Utility.makeClient());
-			Unirest.setDefaultHeader("Authorization", "Bearer " + accessToken );
-			HttpResponse<JsonNode> jsonResponse = Unirest.delete(BASE_URL + "api/1/logo/"+id)
+			Unirest.setDefaultHeader("Authorization", "Bearer " + accessToken);
+			HttpResponse<String> jsonResponse = Unirest.delete(BASE_URL + "logo/"+id)
 					.header("accept", "application/json")
-					.asJson();
+					.asString();
 
 			if(jsonResponse.getCode()==200){
-				JSONObject jsonResponseBody = jsonResponse.getBody().getObject();
+				String jsonResponseBody = jsonResponse.getBody();
 				System.out.println("The response body is : "+jsonResponseBody );
 			}else {
 				System.out.println("The response is : "+ jsonResponse);
